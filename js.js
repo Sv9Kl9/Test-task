3fc2eeb46f910781abcc7d7a231f483affcea7b2
Svitlana Hrytskiv <91554698+Sv9Kl9@users.noreply.github.com>
2022-11-22 19:58:52 +0200

    version two

--- js.js
+++ js.js
@@ -1,39 +1,20 @@
-window.onload = function(){
-fetch('https://vimeo.com/api/oembed.json?url=https%3A//vimeo.com/772410430/dfaaee220c&width=640&height=360&autoplay=1&loop=1&autopause=0&controls=0&muted=1')
-.then(response => response.json())
-.then(vid => {
-  let body = document.querySelector('body');
-  let iframe = document.createElement('div');
-  iframe.innerHTML = vid.html;
-  body.append(iframe);
-} )
-
-fetch('https://vimeo.com/api/oembed.json?url=https%3A//vimeo.com/772410499/dc0b01c09d&width=640&height=360&autoplay=1&loop=1&autopause=0&controls=0&muted=1')
-.then(response => response.json())
-.then(vid => {
-  let body = document.querySelector('body');
-  let iframe = document.createElement('div');
-  iframe.innerHTML = vid.html;
-  body.append(iframe);
-} )
-}
 
 // fetch('https://vimeo.com/api/oembed.json?url=https%3A//vimeo.com/772410430/dfaaee220c&width=640&height=360&autoplay=1&loop=1&autopause=0&controls=0&muted=1')
 // .then(response => response.json())
 // .then(vid => {
 //   let body = document.querySelector('body')
-//   let iframe = document.createElement('div')
-//   iframe.innerHTML = vid.html
-//   body.append(iframe)
-//  
+//   let iframeOne = document.createElement('div')
+//   iframeOne.innerHTML = vid.html
+//   body.append(iframeOne)
+ 
 // } )
 
 // fetch('https://vimeo.com/api/oembed.json?url=https%3A//vimeo.com/772410499/dc0b01c09d&width=640&height=360&autoplay=1&loop=1&autopause=0&controls=0&muted=1')
 // .then(response => response.json())
 // .then(vid => {
 //   let body = document.querySelector('body')
-//   let iframe = document.createElement('div')
-//   iframe.innerHTML = vid.html
-//   body.append(iframe)
-//   
+//   let iframeTwo = document.createElement('div')
+//   iframeTwo.innerHTML = vid.html
+//   body.append(iframeTwo)
+  
 // } )
